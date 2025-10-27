4fc58d6320c670ba3417487a7335315917c631f4
Sathi Ghatuary <sathighatuary@gmail.com>
2025-10-27 08:01:44 +0530

    Number of laser beam in bank

---
+++ Num_of_beam.java
@@ -0,0 +1,20 @@
+public class Num_of_beam{
+    public int numberBeam(String[] bank){
+        long ans=0;
+        long prev=0;
+        for(String row:bank){
+            long cnt=0;
+            for(int i=0;i<row.length();i++){
+                if(row.charAt(i)=='1'){
+                    cnt++;
+                }
+
+            }
+            if(cnt>0){
+                ans+=prev*cnt;
+                prev=cnt;
+            }
+        }
+        return (int)ans;
+    }
+}
