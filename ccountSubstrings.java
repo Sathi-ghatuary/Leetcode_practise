8a7df065b2fc16b73b2e1a0a6c40c4b5cb9b4f36
Sathi Ghatuary <sathighatuary@gmail.com>
2026-01-06 18:29:42 +0530

    Max level sum of Binary Tree

---
+++ ccountSubstrings.java
@@ -0,0 +1,27 @@
+public class ccountSubstrings {
+    public int countsubstrings(String s) {
+        int count=0;
+        if(s==null||s.length()<2){
+            return s.length();
+        }
+        for(int i=0;i<s.length();i++){
+            int left=i;
+            int right=i;
+            while(left>=0&&right<s.length()&&s.charAt(left)==s.charAt(right)){
+                count++;
+                left--;
+                right++;
+            }
+            left = i;
+            right = i + 1;
+            while (left >= 0 && right < s.length()
+                    && s.charAt(left) == s.charAt(right)) {
+                        count++;
+                left--;
+                right++;
+            }
+        }
+        return count;
+
+        }
+}
---
+++ maxMatrixSum.java
@@ -0,0 +1,21 @@
+public class maxMatrixSum {
+    public long maxMatrixsum(int[][] matrix) {
+        int n=matrix.length;
+        int leastElement=100001;
+        long sum=0L;
+        int negativeCount=0;
+        for(int[] rows:matrix){
+            for(int value : rows){
+                if(value < 0){
+                    negativeCount++;
+                    value = -value;
+        }
+            sum += value;
+            if(value < leastElement){
+                    leastElement = value;
+                }
+        
+    }
+}return (negativeCount%2 == 0) ? sum : (sum - 2*leastElement);
+    }
+}
