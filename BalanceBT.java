3b13fdf42c7d27508adc75fc5e4d02d40e705c69
Sathi Ghatuary <sathighatuary@gmail.com>
2026-02-18 19:01:25 +0530

    Binary Alternative bits

---
+++ BalanceBT.java
@@ -0,0 +1,25 @@
+import javax.swing.tree.TreeNode;
+
+public class BalanceBT {
+    private int dfs(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        int leftHeight=dfs(root.left);
+        if(leftHeight==-1){
+            return -1;
+        }
+        int rightHeight=dfs(root.right);
+        if(rightHeight==-1){
+            return -1;
+        }
+        if(Math.abs(leftHeight-rightHeight)>1){
+            return -1;
+        }
+        return 1+Math.max(leftHeight,rightHeight);
+    }
+    public boolean isBalanced(TreeNode root) {
+        return dfs(root)!=-1;
+    }
+}
+
---
+++ Remove_duplicatefromsorted.java
@@ -0,0 +1,13 @@
+public class Remove_duplicatefromsorted {
+    public int removedup(int[] nums){
+        int i=1;
+        for(int j=1;j<nums.length;j++){
+          if(nums[j]!=nums[i-1]){
+            nums[i]=nums[j];
+              i++;
+        
+          }
+        }
+        return i;
+    }
+}
